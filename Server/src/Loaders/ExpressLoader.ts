20dcd1e9ee51f082f2aabec362ef5a05351b1f31
Ravinder <ravinder.7987@gmail.com>
2024-12-31 11:27:22 +0800

    added docker compose and production grade dockerfiles for individual containers

--- Server/src/Loaders/ExpressLoader.ts
+++ Server/src/Loaders/ExpressLoader.ts
@@ -1,46 +1,48 @@
-import Express, { Request, Response } from "express";
-import Cors from "cors";
-import Config from "./ConfigLoader";
-import Version1RoutesHandler from "../Routes/Version1/Index";
+import Express, { Request, Response } from 'express';
+import Cors from 'cors';
+import Config from './ConfigLoader';
+import Version1RoutesHandler from '../Routes/Version1/Index';
 
 /* Initializes the express server application class */
 class ExpressApplication {
-	/* static property express */
-	public static express: Express.Application;
+    /* static property express */
+    public static express: Express.Application;
 
-	/* static fucnction init, which will be used to start the server with necessary configuration */
-	public static init() {
-		this.express = Express();
+    /* static fucnction init, which will be used to start the server with necessary configuration */
+    public static init() {
+        this.express = Express();
 
-		/* parser configuration */
-		this.express.use(
-			Express.json({
-				limit: "16mb",
-			})
-		);
+        /* parser configuration */
+        this.express.use(
+            Express.json({
+                limit: '16mb',
+            }),
+        );
 
-		this.express.use(Express.urlencoded({ extended: true }));
+        this.express.use(Express.urlencoded({ extended: true }));
 
-		/* cors configuration */
-		this.express.use(Cors({
-			credentials: true,
-			origin: ["http://localhost:5173"]
-		}));
+        /* cors configuration */
+        this.express.use(
+            Cors({
+                credentials: true,
+                origin: ['http://localhost:5173'],
+            }),
+        );
 
-		/* Routes configuration */
-		this.express.use("/v1", Version1RoutesHandler());
+        /* Routes configuration */
+        this.express.use('/v1', Version1RoutesHandler());
 
-		/* listens to specified port for server */
-		this.express.listen(
-			{
-				port: Config.env.serverPort,
-				host: "0.0.0.0",
-			},
-			() => {
-				console.log(`express server is up and running on port ${Config.env.serverPort}`);
-			}
-		);
-	}
+        /* listens to specified port for server */
+        this.express.listen(
+            {
+                port: Config.env.serverPort,
+                host: '0.0.0.0',
+            },
+            () => {
+                console.log(`express server is up and running on port ${Config.env.serverPort}`);
+            },
+        );
+    }
 }
 
 export default ExpressApplication;
